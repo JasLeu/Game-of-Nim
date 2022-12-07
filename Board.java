0b2baf70b84a3488a3d73bccce3a5e2130f80b3f
JasLeu <jml858@hotmail.com>
2022-12-07 14:43:12 -0800

    12/7/2022

--- Board.java
+++ Board.java
@@ -1,14 +1,20 @@
+/**
+ * Represents the entire board 
+ */
 public class Board {
-    static int pieces;
+    private static int pieces;
 
+    // sets total pieces
     public static void populate() {
         pieces = (int) (Math.random() * 41 + 10);
     }
 
+    // getters
     public static int getPieces(){
         return pieces;
     }
 
+    // updates score
     public static void updateTotal(int num) {
         pieces -= num;
     }
--- Game.java
+++ Game.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
+/***
+ * Names: Jason/Anchal/Palak
+ * Course: APCSA Tri 2
+ * 
+ * Description: Runs a two player game where each player grabs pieces from the pile.
+ */
 public class Game {
     
+    //declares variables to be used
     Scanner sc = new Scanner(System.in);
     Player p1;
     Player p2;
@@ -11,33 +18,31 @@ public class Game {
 
     // Constructors
     public Game() {
+        // initializes p1 and p2
         System.out.print("Player 1 name: ");
         p1 = new Player();
         System.out.print("Player 2 name: ");
         p2 = new Player();
     
-        
+        // decides who goes first
         currentPlayer = (int) (Math.random() * 2 + 1);
-        // System.out.println(currentPlayer);
     }
 
     // Methods
     public void play() {
-        //Board board = new Board();
+        
+        // sets the initial amount of pieces to take
         pieces = Board.getPieces();
-        System.out.println("\nTotal pieces: " + Board.getPieces() + "\n");
-        /* System.out.println(p1 + ", how many pieces would you like to grab?");
-        grab = Integer.parseInt(sc.nextLine()); */
 
+        // runs the game through a while loop
         while (true) {
             //prints intial question
-            /* System.out.println(currentPlayer + ", how many pieces would you like to grab?");
-            grab = Integer.parseInt(sc.nextLine()); */
-            System.out.println("Pieces: " + Board.getPieces());
+            System.out.println("\nPieces: " + Board.getPieces());
+
             //player 1
             if (currentPlayer == 1) {
 
-
+                // runs if total pieces isn't 1
                 if (Board.getPieces() != 1) {
 
                     //checks for valid input
@@ -47,18 +52,26 @@ public class Game {
                     }
                     while ((grab > Board.getPieces()/2));
 
-
-                    System.out.println("1");
+                    // updates the total pieces in Board
                     Board.updateTotal(grab);
+
+                    // changes the current player to player 2
                     currentPlayer = 2;
 
 
                 } else {
+                    // if the total amount of pieces = 1
+                    
+                    // adds one point to p2
                     p2.setPoints();
-                    System.out.println(p1 + " loses. " + p1 + " has " + p1.getPoints() + " points. " + p2 + " has " + p2.getPoints() + " points. \n\n Would you want to play again?");
-                    if (sc.nextLine().equals("yes")) {
-                        Game nim = new Game(); 
-                        nim.play();
+
+                    // prints the scores
+                    System.out.println(p1 + " loses. " + p1 + " has " + p1.getPoints() + " points. " + p2 + " has " + p2.getPoints() + " points. \n\nWould you want to play again?");
+                    String ans = sc.nextLine();
+
+                    // checks if the player wants to restart the game, if yes then adds pieces to Board, otherwise ends the program
+                    if (ans.equals("yes") || ans.equals("y")) {
+                        Board.populate();
                     } else {
                         System.exit(0);
                     }
@@ -74,17 +87,17 @@ public class Game {
                     }
                     while ((grab > Board.getPieces()/2));
 
-                    System.out.println("2");
+                    //System.out.println("2");
                     Board.updateTotal(grab);
                     currentPlayer = 1;
                 } else {
                     p1.setPoints();
-                    System.out.println(p2 + " loses. " + p2 + " has " + p2.getPoints() + " points. " + p1 + " has " + p1.getPoints() + " points. \n\n Would you want to play again?");
-                    if (sc.nextLine().equals("yes")) {
-                        Game nim = new Game(); 
-                        nim.play();
+                    System.out.println(p2 + " loses. " + p2 + " has " + p2.getPoints() + " points. " + p1 + " has " + p1.getPoints() + " points. \n\nWould you want to play again?");
+                    String ans = sc.nextLine();
+                    if (ans.equals("yes") || ans.equals("y")) {
+                        Board.populate();
                     } else {
-                       System.exit(1);
+                        System.exit(0);
                     }
                 }
             }
