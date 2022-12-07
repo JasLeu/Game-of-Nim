b1926737630a153d241ab4f0d7af2989b2120cdf
JasLeu <jml858@hotmail.com>
2022-12-07 14:43:31 -0800

    12/07/22

--- GameRunner.java
+++ GameRunner.java
@@ -7,10 +7,12 @@ public class GameRunner
 {
   public static void main(String[] args)
   {
+    // welcome message
     System.out.println("Welcome to the Game of Nim!");
 
     Board.populate(); // static method call
     
+    // begins a new game
     Game nim = new Game(); 
     nim.play();
     
--- Player.java
+++ Player.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
+/***
+ * Player class
+ */
 public class Player {
     Scanner sc = new Scanner(System.in);
 
     private String name;
     private int points = 0;
     
+    //initializes name
     public Player() {
         name = sc.nextLine();
     }
 
+    //adds one point
     public void setPoints() {
         points++;
     }
+    
     /*** Getters */
     public String getName() {
         return name;
