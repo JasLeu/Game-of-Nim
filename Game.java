89861227e9518a21f6c81f7d0ed22e87aa4a9bf8
JasLeu <jml858@hotmail.com>
2022-12-08 14:27:13 -0800

    12/08/2022

--- Game.java
+++ Game.java
@@ -12,7 +12,8 @@ public class Game {
     Scanner sc = new Scanner(System.in);
     Player p1;
     Player p2;
-    int currentPlayer;
+    int option;
+    Player currentPlayer;
     int pieces;
     int grab;
 
@@ -21,11 +22,23 @@ public class Game {
         // initializes p1 and p2
         System.out.print("Player 1 name: ");
         p1 = new Player();
-        System.out.print("Player 2 name: ");
-        p2 = new Player();
+        System.out.println("Is there a second player?");
+        String ans = sc.nextLine();
+        if (ans.equals("yes") || ans.equals("y")) {
+            System.out.print("Player 2 name: ");
+            p2 = new Player();
+        } else {
+            p2 = new Player("Bot");
+        }
+        
     
         // decides who goes first
-        currentPlayer = (int) (Math.random() * 2 + 1);
+        option = Player.choosePlayer();
+        if (option == 1) {
+            currentPlayer = p1;
+        } else {
+            currentPlayer = p2;
+        }
     }
 
     // Methods
@@ -36,74 +49,47 @@ public class Game {
 
         // runs the game through a while loop
         while (true) {
-            //prints intial question
+            // prints intial question
             System.out.println("\nPieces: " + Board.getPieces());
 
-            //player 1
-            if (currentPlayer == 1) {
-
-                // runs if total pieces isn't 1
-                if (Board.getPieces() != 1) {
-
-                    //checks for valid input
+            // runs if total pieces isn't 1
+            if (Board.getPieces() != 1) {
+                
+                // runs if there is a second player
+                if (!currentPlayer.getName().equals("Bot")) {
                     do {
-                        System.out.println(p1 + ", how many pieces would you like to grab?");
+                        System.out.println(currentPlayer + ", how many pieces would you like to grab?");
                         grab = Integer.parseInt(sc.nextLine());
-                    }
-                    while ((grab > Board.getPieces()/2));
-
-                    // updates the total pieces in Board
+                    } while (grab > Board.getPieces()/2);
                     Board.updateTotal(grab);
+                    currentPlayer = p2;
 
-                    // changes the current player to player 2
-                    currentPlayer = 2;
-
-
+                // runs if there isn't a second player
                 } else {
-                    // if the total amount of pieces = 1
-                    
-                    // adds one point to p2
-                    p2.setPoints();
-
-                    // prints the scores
-                    System.out.println(p1 + " loses. " + p1 + " has " + p1.getPoints() + " points. " + p2 + " has " + p2.getPoints() + " points. \n\nWould you want to play again?");
-                    String ans = sc.nextLine();
-
-                    // checks if the player wants to restart the game, if yes then adds pieces to Board, otherwise ends the program
-                    if (ans.equals("yes") || ans.equals("y")) {
-                        Board.populate();
-                    } else {
-                        System.exit(0);
-                    }
-                }            
-    
-
-            //player 2
-            } else {
-                if(Board.getPieces() != 1) {
-                    do {
-                        System.out.println(p2 + ", how many pieces would you like to grab?");
-                        grab = Integer.parseInt(sc.nextLine());
-                    }
-                    while ((grab > Board.getPieces()/2));
-
-                    //System.out.println("2");
+                    grab = currentPlayer.randomizeNumber();
+                    System.out.println(currentPlayer + " chooses " + grab + ".");
                     Board.updateTotal(grab);
-                    currentPlayer = 1;
-                } else {
-                    p1.setPoints();
-                    System.out.println(p2 + " loses. " + p2 + " has " + p2.getPoints() + " points. " + p1 + " has " + p1.getPoints() + " points. \n\nWould you want to play again?");
-                    String ans = sc.nextLine();
-                    if (ans.equals("yes") || ans.equals("y")) {
-                        Board.populate();
-                    } else {
-                        System.exit(0);
-                    }
+                    currentPlayer = p1;  
                 }
+            } else {
+
+                // checks if the current player chosen was bot, indicating that they have the last piece
+                if (currentPlayer.getName().equals("Bot")) {
+                    p1.setPoints();
+                    System.out.println(p2 + " loses. " + p2 + " has " + p2.getPoints() + " points. " + p1 + " has " + p1.getPoints() + " points. \n\nWould you want to play again?");   
+                } else {
+                    p2.setPoints();
+                    System.out.println(p1 + " loses. " + p1 + " has " + p1.getPoints() + " points. " + p2 + " has " + p2.getPoints() + " points. \n\nWould you want to play again?");
+                }
+                String ans = sc.nextLine();
+
+                // checks if the player wants to restart the game, if yes then adds pieces to Board, otherwise ends the program
+                if (ans.equals("yes") || ans.equals("y")) {
+                    Board.populate();
+                } else {
+                    System.exit(0);
+                } 
             }
         }
-        
-        
-        
     }
 }
